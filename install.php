fa367b1904b36092fffa4a9e2ed231a90abbd766
Pasi Kallinen <paxed@alt.org>
2011-07-27 20:08:25 +0300

    Improve the install script

--- install.php
+++ install.php
@@ -1,5 +1,18 @@
 <?php
 
+function write_settings($fname, $data)
+{
+    $fp = fopen($fname,"w");
+    $str = "<?php \n";
+    foreach ($data as $key=>$val) {
+	$str .= $key.' = '.$val.";\n";
+    }
+    if (fwrite($fp, $str, strlen($str)) === FALSE) {
+	return FALSE;
+    }
+    return TRUE;
+}
+
 $def_template = './templates/bash_template/bash_template.php';
 
 if (file_exists($def_template)) {
@@ -9,86 +22,82 @@ if (file_exists($def_template)) {
     function printfooter() {}
 }
 
-if (file_exists('settings.php')){
-    die("settings.php already exists.");
-}
-
 printheader();
 
 If($_SERVER['QUERY_STRING'] == md5('create_file')){
-	$fp = fopen("settings.php","w");
-	$string = "<?php \n"
-				." // Server information\n"
-				."//\n"
-				."\$template			='".$_POST["template"]."';\n"
-				."\$GET_SEPARATOR		= ini_get('arg_separator.output');\n"
-				."\$GET_SEPARATOR_HTML	= htmlspecialchars(\$GET_SEPARATOR, ENT_QUOTES);\n\n"
-				."// Database Information\n"
-				."//\n"
-				."\$phptype	= '".$_POST['phptype']."';\n"
-				."\$hostspec	= '".$_POST['hostspec']."';\n"
-				."\$port		= '';\n"
-				."\$socket		= '';\n"
-				."\$database	= '".$_POST['database']."';\n"
-				."\$username	= '".$_POST['username']."';\n"
-				."\$password	= '".$_POST['password']."';\n\n"
-				."// Other Information\n"
-				."//\n"
-//				."\$language	= '".$_POST['language']."';\n"
-				."?>";
-	if (fwrite($fp, $string, strlen($string)) === FALSE) {
-	    die("Sorry, cannot write settings.php");
-	}
-	header("Location: install.php?".md5('create_tables'));
+    if (file_exists('settings.php')){
+	die("settings.php already exists.");
+    }
+    $data = array('$template' => "'".$_POST['template']."'",
+		  '$GET_SEPARATOR' => "ini_get('arg_separator.output')",
+		  '$GET_SEPARATOR_HTML' => 'htmlspecialchars($GET_SEPARATOR, ENT_QUOTES)',
+		  '$phptype' => "'".$_POST['phptype']."'",
+		  '$hostspec' => "'".$_POST['hostspec']."'",
+		  '$port' => "''",
+		  '$socket' => "''",
+		  '$database' => "'".$_POST['database']."'",
+		  '$username' => "'".$_POST['username']."'",
+		  '$password' => "'".$_POST['password']."'",
+		  '$rss_url' => "'".$_POST['rss_url']."'",
+		  '$rss_title' => "'".$_POST['rss_title']."'",
+		  '$language' => "'US-english'",
+		  '$quote_limit' => '10',
+		  '$page_limit' => '5');
+    if (!write_settings('settings.php', $data)) {
+	die("Sorry, cannot write settings.php");
+    }
+    header("Location: install.php?".md5('create_tables'));
 }
 elseif($_SERVER['QUERY_STRING'] == md5('create_tables')){
-	$res =& $db->query("CREATE TABLE rash_quotes (
-							id int(11) NOT NULL auto_increment primary key,
+    if (!file_exists('settings.php')){
+	die("settings.php does not exist.");
+    }
+
+    print '<h2>Creating database tables...</h2>';
+
+
+    function mk_db_table($tablename,$fields)
+    {
+	include 'settings.php';
+	include 'connect.php';
+	print 'Create table '.$tablename.': ';
+	$sql = 'CREATE TABLE '.$tablename.' ('.$fields.');';
+	$res =& $db->query($sql);
+	if (DB::isError($res)) {
+	    die($res->getMessage());
+	}
+	echo "OK<br />";
+    }
+
+    mk_db_table('rash_quotes', "id int(11) NOT NULL auto_increment primary key,
 							quote text NOT NULL,
 							rating int(7) NOT NULL,
 							flag int(1) NOT NULL,
-							date int(10) NOT NULL); ");
-	if (DB::isError($res)) {
-	    die($res->getMessage());
-	}
-	echo "Table rash_quotes has been created successfully!<br />";
-	$res =& $db->query("CREATE TABLE rash_queue (
-							id int(11) NOT NULL auto_increment primary key,
-							quote text NOT NULL); ");
-	if (DB::isError($res)) {
-	    die($res->getMessage());
-	}
-	echo "Table rash_queue has been created successfully!<br />";
-	$res =& $db->query("CREATE TABLE rash_tracking (
-							id int(11) NOT NULL auto_increment primary key,
+							date int(10) NOT NULL");
+
+    mk_db_table('rash_queue', "id int(11) NOT NULL auto_increment primary key,
+							quote text NOT NULL");
+
+
+    mk_db_table('rash_tracking', "id int(11) NOT NULL auto_increment primary key,
 							ip varchar(15) NOT NULL,
 							quote_id text NOT NULL,
 							vote text NOT NULL,
-							flag text NOT NULL); ");
-	if (DB::isError($res)) {
-	    die($res->getMessage());
-	}
-	echo "Table rash_tracking has been created successfully!<br />";
-	$res =& $db->query("CREATE TABLE rash_users (
-							user varchar(20) NOT NULL,
+							flag text NOT NULL");
+
+    mk_db_table('rash_users', "user varchar(20) NOT NULL,
 							`password` varchar(255) NOT NULL,
 							level int(1) NOT NULL,
-							salt text); ");
-	if (DB::isError($res)) {
-	    die($res->getMessage());
-	}
-	echo "Table rash_users has been created successfully!<br />";
-	$res =& $db->query("CREATE TABLE rash_news (
-							id int(11) NOT NULL auto_increment primary key,
+							salt text");
+
+    mk_db_table('rash_news', "id int(11) NOT NULL auto_increment primary key,
 							news text NOT NULL,
-							date int(10) NOT NULL); ");
-	if (DB::isError($res)) {
-	    die($res->getMessage());
-	}
-	echo "Table rash_news has been created successfully!<br />";
-	echo 'Everything should now be OK.';
+							date int(10) NOT NULL");
+
+    echo 'Everything should now be OK.';
 }
-elseif(!file_exists('settings.php')){
+else {
+    if(!file_exists('settings.php')){
 ?>
 <h2>Install</h2>
 <pre><form action="?<?=md5('create_file')?>" method="post">
@@ -99,15 +108,17 @@ elseif(!file_exists('settings.php')){
   DB Username:	      <input type="text" name="username" value="username">
   DB Password:	      <input type="password" name="password" value="password">
 
-<!--  Language: <select name="language">
-   <option value="US-English">US-English
-  </select><br />-->
+  RSS URL:            <input type="text" name="rss_url" value="<?php echo 'http://'.$_SERVER['SERVER_NAME'];?>">
+  RSS Title:          <input type="text" name="rss_title" value="Rash QDB">
+
   <input type="submit" value="Submit">
  </form>
 </pre>
 <?php
+    } else {
+	die("settings.php already exists.");
+    }
 }
-
 printfooter();
 
 ?>
