bba941c29b0ab6a0dd88bd6f9cf5ae357a921dfb
Pasi Kallinen <paxed@alt.org>
2011-09-14 19:07:32 +0300

    Remove the nhqdb google analytics code

--- templates/nhqdb/nhqdb.php
+++ templates/nhqdb/nhqdb.php
@@ -16,20 +16,6 @@ ob_start();
  <style type="text/css" media="all">
   @import "./templates/nhqdb/style.css";
  </style>
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-24280817-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-
-</script>
 <script src="./templates/nhqdb/util.js" type="text/javascript"></script>
 </head>
 <body>
